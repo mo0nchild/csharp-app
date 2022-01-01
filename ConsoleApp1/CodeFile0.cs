95506ab7eec601d0ed04297279b347177216d266
mo0nchild <letsplaytoday778@gmail.com>
2022-01-01 19:34:35 +0300

    2

---
+++ ConsoleApp1/CodeFile0.cs
@@ -0,0 +1,70 @@
+﻿namespace MyApp;
+
+public class Task 
+{
+    public Func<Func<int>> SEE = () =>
+    {
+        int shit() => 12;
+        return shit;
+    };
+
+    public int Testc(Func<int, int> sfa, int s)
+    {
+        return s + sfa(s);
+    }
+    public static void Run()
+    {
+        var (shit, asd) = new Usser { FirstName = 123, LastName = "23" };
+        Console.WriteLine("Hello World!" + $"\t{shit}\t{asd}");
+
+        var prog = new Task();
+        Console.WriteLine(prog.SEE()());
+
+        Console.WriteLine(prog.Testc((int s) => {
+            return 12 + s;
+        }, 20));
+
+        unsafe
+        {
+            int a = 100;
+            int* p = &a;
+            (*p)++;
+
+
+            Console.WriteLine($"{(*p)} {a}");
+        }
+
+
+        new List<int> { 1123, 2123, 3123, 4234 }.ForEach((e) => {
+            Console.WriteLine(e);
+        });
+
+
+        User user = new Human("Mike", "shit@gmail.com", 14);
+        Console.WriteLine(user.Get_Data());
+        user.Print_Info();
+
+
+
+
+        var test = new TestEvent(120);
+        test.SendEvent += GetInfo;
+        test.SendEvent += (int ID) => {
+            Console.WriteLine("\t\tASDASDASDASDASds" + ID);
+        };
+        test.Doshit();
+
+        var sss = new
+        {
+            Name = "TOm",
+            Age = 123,
+        };
+    }
+
+    public static void GetInfo(int ID)
+    {
+        Console.WriteLine($"current ID: {ID}");
+    }
+}
+
+
---
+++ ConsoleApp1/CodeFile1.cs
@@ -0,0 +1,64 @@
+﻿
+namespace MyApp;
+
+delegate void EventTest(int ID);
+class TestEvent
+{
+    public event EventTest SendEvent;
+
+    public int ID { get; set; }
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+    public TestEvent(int ID)
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+    {
+        this.ID = ID;
+    }
+
+    public void Doshit()
+    {
+        this.ID += 8;
+        SendEvent(this.ID);
+    }
+
+}
+
+public abstract class User
+{
+    protected string name;
+    protected string email;
+    protected uint age;
+    public User(string name, string email, uint age)
+    {
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+    public virtual void Print_Info()
+    {
+        Console.WriteLine(@$"
+        {age}->
+        {name} == {email}");
+    }
+    public abstract object Get_Data();
+}
+
+public class Human : User
+{
+    public uint phone;
+    public Human(string name, string email, uint age) : base(name, email, age)
+    {
+        this.phone = age + (uint)Math.Pow(10, 4);
+    }
+    public override object Get_Data()
+    {
+        return phone;
+    }
+    public override void Print_Info()
+    {
+        base.Print_Info();
+        Console.WriteLine($"{this.phone}\t->\t{this.name}");
+    }
+}
+
+public record struct Usser(int FirstName, string LastName);
