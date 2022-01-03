ee4c8e0194f349cc398b7ff82310ce06ad2914de
mo0nchild <letsplaytoday778@gmail.com>
2022-01-04 02:31:09 +0300

    new

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MyProgram;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        int index = 0;
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine(arr[index++]);
+            }
+        }
+        catch  
+        {
+            Console.WriteLine(index);
+        }
+        
+    }
+}
