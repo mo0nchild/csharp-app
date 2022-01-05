81dcc241d07cafa82976c1aeeba8cb436a792869
mo0nchild <letsplaytoday778@gmail.com>
2022-01-05 04:53:00 +0300

    ihne

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,14 +1,47 @@
-﻿using MyLib;
-namespace MyProgram;
+﻿namespace MyProgram;
+
+class Person
+{
+    protected uint age;
+    public uint Age 
+    {
+        get => this.age;
+        set 
+        {
+            if ((age = value) >= 100) this.IsDead = true;        
+        }
+    }
+    public string Name { get; init; }
+    public bool IsDead { get; private set; } = false;
+    public Person(string name = "none", uint age = 0) 
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
+
+class Client : Person
+{
+    private static uint counter = 0;
+    public uint Id { get; set; } = 0;
+    public Client(string name, uint age) : base(name, age)
+    {
+        this.Id = ++counter;
+    }
+}
+
+class Admin : Person 
+{
+    public Admin(string name, uint age) : base(name, age)
+    { }
+}
 
 public class Program
 {
     
     public static void Main(string[] args)
     {
-        
-        var steve = new User("Steve", 18);
-        Console.WriteLine(steve.Email);
-        
+        Client mike = new("Mike", 32);
+        if (mike is Person mike_person) Console.WriteLine(mike_person.Name);
     }
 }
