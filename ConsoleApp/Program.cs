6fe3fb83390a5bd8431c52e3f776953606d9c459
mo0nchild <letsplaytoday778@gmail.com>
2022-01-07 01:35:16 +0300

    exception

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ class Messenger<T> where T : Message
 {
     public static void SendMessage(T msg) => Console.WriteLine(msg.MessageText);
 }
+
 abstract class Message
 {
     public string MessageText { get; init; }
@@ -26,10 +27,34 @@ class EmailMessage : Message
 
 public class Program
 {
+    static void Check(int m = 0)
+    {
+        try
+        {
+            throw new TestException("Exception Message", "Value");
+        }
+        catch (TestException e) 
+        {
+            Console.WriteLine($"{e.Value}");
+        }
+        catch (Exception e) when (m == 0)
+        {
+            Console.WriteLine($"m=0\t{e.Message}");
+        }
+    }
     public static void Main(string[] args)
     {
-        Console.Write("Input message: ");
+        Check(0);
+
+        /*Console.Write("Input message: ");
         string? msg = Console.ReadLine();
-        Messenger<EmailMessage>.SendMessage(new EmailMessage($"Hello, {msg}", "test@gmail.com"));
+        Messenger<EmailMessage>.SendMessage(new EmailMessage($"Hello, {msg}", "test@gmail.com"));*/
     }
+}
+
+class TestException : Exception 
+{
+    public string Value { get; init; }
+    public TestException(string msg, string value)
+        : base(msg) => Value = value;
 }
